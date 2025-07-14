0d1752ce4b9dfa17f29278853256d844fb4c79dc
rahim4shanewaz <soumiq444@gmail.com>
2025-07-15 05:24:41 +0600

    login form added and get value from pin and number input

---
+++ js/login.js
@@ -0,0 +1,26 @@
+
+//phone number and Pin
+
+document.getElementById('btn-login')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        //geting phone number
+        const phoneNumber = document.getElementById('phone-number').value;
+
+        console.log(phoneNumber)
+
+        //getting Pin
+        const pinNumber = document.getElementById('pin').value;
+
+        console.log(pinNumber);
+
+        if(phoneNumber === "1111" && pinNumber === "1234"){
+            console.log("loggd in")
+
+        }
+        else{
+            alert("wrong Phone number or Pin")
+
+        }
+        } )
